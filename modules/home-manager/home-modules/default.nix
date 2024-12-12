cc033454377989e0f2ced4268b664b65d1d2b301
FrankStrix <147439332+FrankStrix@users.noreply.github.com>
2024-12-12 10:09:12 +0100

    Add files via upload

--- modules/home-manager/home-modules/default.nix
+++ modules/home-manager/home-modules/default.nix
@@ -1,1 +1,15 @@
-
+{
+  imports = [
+      ./hyprland
+      ./swaync/swaync.nix
+      ./waybar
+      ./git.nix
+      ./gtk.nix
+      ./kitty.nix
+      ./packages.nix
+      ./rofi.nix
+      ./session-var.nix
+      ./swaylock.nix
+      ./vscode.nix
+  ];
+}

34e0e927909ada1ba25e06fd442e57433cac5d18
FrankStrix <147439332+FrankStrix@users.noreply.github.com>
2024-12-12 10:12:45 +0100

    Add files via upload

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -1,54 +1,9 @@
-{ config, pkgs, ... }:
+{ config, pkgs, ... }: {
 
-{
-  home.stateVersion = "24.05";  # Don't change without checking release notes
-
-  # User packages
-  home.packages = [
-    pkgs.vscodium
-    pkgs.git
-    pkgs.kitty
-    pkgs.neovim
-    pkgs.zsh
-    pkgs.autojump
-    pkgs.zsh-syntax-highlighting
-    pkgs.zsh-autosuggestions
+  imports = [
+      ./home-modules
   ];
 
-  home.sessionVariables = {
-    EDITOR = "neovim";
-    BROWSER = "google-chrome";
-    TERMINAL = "kitty";
-  };
-
-  programs = {
-    git = {
-      enable = true;
-      userName = "FrankStrix";
-      userEmail = "matteo.francavilla@outlook.com";
-      aliases = {
-        pu = "push";
-        co = "checkout";
-        cm = "commit";
-      };
-      extraConfig = {
-        color.ui = "auto";
-        push.default = "simple";
-        pull.rebase = true;
-        branch.autosetuprebase = "always";
-        init.defaultBranch = "main";
-      };
-    };
-
-    vscode = {
-      enable = true;
-      package = pkgs.vscodium;
-    };
-
-    kitty = {
-      enable = true;
-    };
-  };
-
+  home.stateVersion = "24.05";  # Don't change without checking release notes
   programs.home-manager.enable = true;
 }

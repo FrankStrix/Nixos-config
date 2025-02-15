0532134af64ed0405acca29b97abfdfac4fa4424
FrankStrix <147439332+FrankStrix@users.noreply.github.com>
2025-02-15 21:33:01 +0100

    Delete modules/nixos/modules directory

--- modules/nixos/modules/networking.nix
+++
@@ -1,9 +0,0 @@
-{
-    networking = {
-        networkmanager.enable = true;
-        hostName = "nixos";
-        extraHosts = ''
-        10.129.199.47 unika.htb
-        '';
-  };
-}
--- modules/nixos/modules/users.nix
+++
@@ -1,7 +0,0 @@
-{
-  users.users.frank = {
-    isNormalUser = true;
-    description = "Frank";
-    extraGroups = [ "networkmanager" "wheel" ];
-  };
-}
